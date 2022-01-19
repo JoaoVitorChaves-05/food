d9588ecc35426b6db7d0829d2c9bcca4de09bf55
João Vitor Mancio Chaves <joaovmanciochaves@gmail.com>
2022-01-19 14:17:55 -0300

    update

--- public/scripts/links.js
+++ public/scripts/links.js
@@ -1,5 +1,4 @@
 const restaurantes = document.querySelectorAll(".item-restaurant")
 restaurantes.forEach(item => item.addEventListener('click', () => {
-    window.location.href += item.childNodes[3].childNodes[1].innerHTML
-    console.log(window.location.href)
+    window.location.href += item.children[2].innerHTML
 }))
